930d991371da8676d3769c7e9b966bee137c0e24
major1balaz525 <major.balazs@telekom.com>
2020-05-03 23:08:53 +0200

    Fix ui.

--- src/main/java/com/example/demo/ui/MainView.java
+++ src/main/java/com/example/demo/ui/MainView.java
@@ -4,25 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.data.UserDetails;
 import com.example.demo.data.UserDetailsService;
 import com.example.demo.data.UserDetailsService.ServiceException;
-import com.example.demo.ui.components.AvatarField;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.EmailField;
-import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.ValidationException;
-import com.vaadin.flow.data.binder.ValidationResult;
-import com.vaadin.flow.data.binder.ValueContext;
-import com.vaadin.flow.data.validator.EmailValidator;
 import com.vaadin.flow.router.Route;
 
 /**
@@ -34,17 +27,8 @@ import com.vaadin.flow.router.Route;
 @Route("")
 public class MainView extends VerticalLayout {
 
-    private Checkbox allowMarketingBox;
-    private PasswordField passwordField1;
-    private PasswordField passwordField2;
-
     private UserDetailsService service;
     private BeanValidationBinder<UserDetails> binder;
-
-    /**
-     * Flag for disabling first run for password validation
-     */
-    private boolean enablePasswordValidation;
 
     /**
      * We use Spring to inject the backend into our view
@@ -59,40 +43,26 @@ public class MainView extends VerticalLayout {
 
         H3 title = new H3("Bank account");
 
-        TextField firstnameField = new TextField("First name");
-        TextField lastnameField = new TextField("Last name");
-        TextField handleField = new TextField("User handle");
+        TextField idField = new TextField("Identifier of customer");
         
         Button queryButton = new Button("Query Account by Id");
         queryButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        // This is a custom field we create to handle the field 'avatar' in our data. It
-        // work just as any other field, e.g. the TextFields above. Instead of a String
-        // value, it has an AvatarImage value.
-        AvatarField avatarField = new AvatarField("Select Avatar image");
-
-        // We'll need these fields later on so let's store them as class variables
-        allowMarketingBox = new Checkbox("Allow Marketing?");
-        allowMarketingBox.getStyle().set("padding-top", "10px");
-        EmailField emailField = new EmailField("Email");
-        emailField.setVisible(false);
-
-        passwordField1 = new PasswordField("Wanted password");
-        passwordField2 = new PasswordField("Password again");
 
         Span errorMessage = new Span();
 
-        Button submitButton = new Button("Join the community");
-        submitButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        TextField nameField = new TextField("Name");
+        nameField.setReadOnly(true);
+        TextField balanceField = new TextField("Balance");
+        balanceField.setReadOnly(true);
+        
+        TextField incrementField = new TextField("Increment");
+        Button incrementButton = new Button("Increment");
 
-        /*
-         * Build the visible layout
-         */
+        TextField withdrawField = new TextField("Withdraw");
+        Button withdrawButton = new Button("Withdraw");
 
-        // Create a FormLayout with all our components. The FormLayout doesn't have any
-        // logic (validation, etc.), but it allows us to configure Responsiveness from
-        // Java code and its defaults looks nicer than just using a VerticalLayout.
-        FormLayout formLayout = new FormLayout(title, firstnameField, lastnameField, queryButton, handleField, avatarField, passwordField1, passwordField2,
-                allowMarketingBox, emailField, errorMessage, submitButton);
+        FormLayout formLayout = new FormLayout(title, idField, queryButton, nameField,
+                balanceField, incrementField, incrementButton, withdrawField, withdrawButton, errorMessage);
 
         // Restrict maximum width and center on page
         formLayout.setMaxWidth("500px");
@@ -106,9 +76,8 @@ public class MainView extends VerticalLayout {
         // These components take full width regardless if we use one column or two (it
         // just looks better that way)
         formLayout.setColspan(title, 2);
-        formLayout.setColspan(avatarField, 2);
-        formLayout.setColspan(errorMessage, 2);
-        formLayout.setColspan(submitButton, 2);
+        formLayout.setColspan(idField, 2);
+        formLayout.setColspan(queryButton, 2);
 
         // Add some styles to the error message to make it pop out
         errorMessage.getStyle().set("color", "var(--lumo-error-text-color)");
@@ -132,65 +101,10 @@ public class MainView extends VerticalLayout {
         binder = new BeanValidationBinder<UserDetails>(UserDetails.class);
 
         // Basic name fields that are required to fill in
-        binder.forField(firstnameField).asRequired().bind("firstname");
-        binder.forField(lastnameField).asRequired().bind("lastname");
-
-        // The handle has a custom validator, in addition to being required. Some values
-        // are not allowed, such as 'admin'; this is checked in the validator.
-        binder.forField(handleField).withValidator(this::validateHandle).asRequired().bind("handle");
-
-        // Here we use our custom Vaadin component to handle the image portion of our
-        // data, since Vaadin can't do that for us. Because the AvatarField is of type
-        // HasValue<AvatarImage>, the Binder can bind it automatically. The avatar is
-        // not required and doesn't have a validator, but could.
-        binder.forField(avatarField).bind("avatar");
-
-        // Allow marketing is a simple checkbox
-        binder.forField(allowMarketingBox).bind("allowsMarketing");
-        // EmailField uses a Validator that extends one of the built-in ones.
-        // Note that we use 'asRequired(Validator)' instead of
-        // 'withValidator(Validator)'; this method allows 'asRequired' to
-        // be conditional instead of always on. We don't want to require the email if
-        // the user declines marketing messages.
-        binder.forField(emailField).asRequired(new VisibilityEmailValidator("Value is not a valid email address")).bind("email");
-
-        // Only ask for email address if the user wants marketing emails
-        allowMarketingBox.addValueChangeListener(e -> {
-
-            // show or hide depending on the checkbox
-            emailField.setVisible(allowMarketingBox.getValue());
-
-            // Additionally, remove the input if the user decides not to allow emails. This
-            // way any input that ends up hidden on the page won't end up in the bean when
-            // saved.
-            if (!allowMarketingBox.getValue()) {
-                emailField.setValue("");
-            }
-        });
-
-        // Another custom validator, this time for passwords
-        binder.forField(passwordField1).asRequired().withValidator(this::passwordValidator).bind("password");
-        // We won't bind passwordField2 to the Binder, because it will have the same
-        // value as the first field when correctly filled in. We just use it for
-        // validation.
-
-        // The second field is not connected to the Binder, but we want the binder to
-        // re-check the password validator when the field value changes. The easiest way
-        // is just to do that manually.
-        passwordField2.addValueChangeListener(e -> {
-
-            // The user has modified the second field, now we can validate and show errors.
-            // See passwordValidator() for how this flag is used.
-            enablePasswordValidation = true;
-
-            binder.validate();
-        });
-
-        // A label where bean-level error messages go
-        binder.setStatusLabel(errorMessage);
+        binder.forField(idField).asRequired().bind("id");
 
         // And finally the submit button
-        submitButton.addClickListener(e -> {
+        queryButton.addClickListener(e -> {
             try {
 
                 // Create empty bean to store the details into
@@ -234,78 +148,5 @@ public class MainView extends VerticalLayout {
         notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);
 
         // Here you'd typically redirect the user to another view
-    }
-
-    /**
-     * Method to validate that:
-     * <p>
-     * 1) Password is at least 8 characters long
-     * <p>
-     * 2) Values in both fields match each other
-     */
-    private ValidationResult passwordValidator(String pass1, ValueContext ctx) {
-
-        /*
-         * Just a simple length check. A real version should check for password
-         * complexity as well!
-         */
-        if (pass1 == null || pass1.length() < 8) {
-            return ValidationResult.error("Password should be at least 8 characters long");
-        }
-
-        if (!enablePasswordValidation) {
-            // user hasn't visited the field yet, so don't validate just yet, but next time.
-            enablePasswordValidation = true;
-            return ValidationResult.ok();
-        }
-
-        String pass2 = passwordField2.getValue();
-
-        if (pass1 != null && pass1.equals(pass2)) {
-            return ValidationResult.ok();
-        }
-
-        return ValidationResult.error("Passwords do not match");
-    }
-
-    /**
-     * Method that demonstrates using an external validator. Here we ask the backend
-     * if this handle is already in use.
-     */
-    private ValidationResult validateHandle(String handle, ValueContext ctx) {
-
-        String errorMsg = service.validateHandle(handle);
-
-        if (errorMsg == null) {
-            return ValidationResult.ok();
-        }
-
-        return ValidationResult.error(errorMsg);
-    }
-
-    /**
-     * Custom validator class that extends the built-in email validator.
-     * <p>
-     * Ths validator checks if the field is visible before performing the
-     * validation. This way, the validation is only performed when the user has told
-     * us they want marketing emails.
-     */
-    public class VisibilityEmailValidator extends EmailValidator {
-
-        public VisibilityEmailValidator(String errorMessage) {
-            super(errorMessage);
-        }
-
-        @Override
-        public ValidationResult apply(String value, ValueContext context) {
-
-            if (!allowMarketingBox.getValue()) {
-                // Component not visible, no validation
-                return ValidationResult.ok();
-            } else {
-                // normal email validation
-                return super.apply(value, context);
-            }
-        }
     }
 }
